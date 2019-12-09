825c2a04451158c9b0f433e28a550d4e60b86757
Nick Anderson <NAnderson@wagstaff.com>
2019-12-09 13:24:36 -0800

    stubbed out the vue page

---
+++ src/components/Timer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container-fluid mt-4">
+    <h1 class="h1">Contact Manger</h1>
+    <b-alert :show="loading" variant="info">Loading...</b-alert>
+    <b-row>
+      <b-col>
+        <b-card>
+          <form @submit.prevent="saveTime">
+            <b-button variant="success">Start</b-button>
+            <b-button variant="success">Stop</b-button>
+          </form>
+        </b-card>
+      </b-col>
+    </b-row>
+  </div>
+</template>
